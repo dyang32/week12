a19bd92ce46beb17960643f7a8a37a037c5f9c3d
dyang32 <dyang32@gmail.com>
2018-04-01 03:05:15 -0500

    second commit

---
+++ routerslide.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+# Week 12
+# Don Yang
+# CSCI 2461-70
+#router slide articles
+
+#Summarize article in a script
+
+#The first step is your browser has to establish a network connection to the machine
+#The browser queries a program called a name server, the name server can live on the machine
+#or on a server somewhere else.
+#The name servers talk to eachother and exchange information keeping up to date on information
+#needed to resolve hostnames.
+
+#DNS is a network of programs ans databases the cooperates to translate hostnames to IP addresses
+#Each domain is defined by a name server that knows the IP address of the other machines in the
+#domain
+
+#Lower level IP is responsible for labeling undividual packets with the source address and destination
+#Upper level TCP gives you reliability
+
+#Web browsers and server are on the application level and runs on top of TCP/IP
+#The protocol is called HTTP
+
---
+++ tcpdump.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+# Week 12
+# Don Yang
+# CSCI 2461-70
+#tcpdump
+
+echo "apply tcpdump toturial in a script"
+echo
+echo "tcpdump is a tool for network analysis, is good for understanding TCP/IP"
+echo
+echo "When using tcpdump you can use the following options (not all are listed)"
+echo " -i any - 	listens on all interfaces"
+echo " -i eth0 - 	listens on the eth0 interface"
+echo " -D - 		show the list of available interfaces"
+echo " -n - 		don't resolve hostnames"
+echo " -X - 		show the packet's contents in HEX and ASCII"
+echo " -S -		define the snaplength"
+echo " -e - 		get the ethernet header as well"
+echo " -q - 		show less protocol information"
+echo
+echo "There are three main types of expression:"
+echo " Type, dir, and proto"
+echo
+echo "Type options are: host, net, and port"
+echo "Direction lets you do src, dst, and combinations thereof"
+echo "Proto(col) lets you designate: tcp, udp, icmp, ah, and many more"
+echo
+
+
+
+
+
+
+
